e58766f17fb9f3087c1d6db104f5ff32d3ae2fa0
theqwertycoder <theqwertycoder@gmail.com>
2024-02-20 11:07:34 -0500

    updated pcb & pricing data

--- main/include/hardware/screen.h
+++ main/include/hardware/screen.h
@@ -3,11 +3,10 @@
 #include <map>
 
 typedef uint8_t Color;
-class Screen {
+class Screen { // screen is written to using a table of 256 
 	Screen();
 	static const int width, height;
 	void vSync();
-	std::array<Color, 240*320> buffer; // screen resolution should be 320x240, preferably 96dpi
 	void write(Color *buf, int x, int y, int w, int h);
 	void drawLine(int x1, int y1, int x2, int y2, Color color);
 	void drawBox(int x1, int y1, int x2, int y2, Color outline);
@@ -15,5 +14,6 @@ class Screen {
 	void drawFilledBox(int x1, int y1, int x2, int y2, Color fill, Color outline);
 	void drawCircle(int x, int y, int radius);
 	void drawEllipse(int x1, int y1, int x2, int y2);
+	void drawBitmap();
 };
 
